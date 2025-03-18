b1321482b9070e994aae6bdadea197efb6186024
sanjanam2107 <sanjanamanjunath6363@gmail.com>
2025-03-18 16:33:33 +0530

    edited hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello,PES1UG22CS527\n";
-  cout << "Hello, Jenkins yayy\n";
+  cout << "Hello, Jenkins yayyy\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
